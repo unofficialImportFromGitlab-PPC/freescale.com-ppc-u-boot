b08de072367f29e4b8cbaa5b6bc899c1fb1de498
Lei Xu <B33228@freescale.com>
2011-05-06 14:18:35 +0800

    powerpc/85xx: Add workaround for erratum USB-138 (enable on P204x/P3041/P50x0)
    USB transmit will result internal multi-bit ECC, errors, which has impact
    on performance, so software should disable all ECC reporting from USB1
    and USB2 by setting bits 16 and 17 to 1 in the register at
    DCSRBASE + 0x0002_0520.
    
    Signed-off-by: Lei Xu <B33228@freescale.com>
    Signed-off-by: Roy Zang <tie-fei.zang@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/cpu/mpc85xx/cmd_errata.c
+++ arch/powerpc/cpu/mpc85xx/cmd_errata.c
@@ -113,6 +113,9 @@ static int do_errata(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	if ((SVR_MAJ(svr) == 1) || IS_SVR_REV(svr, 2, 0))
 		puts("Work-around for Erratum NMG ETSEC129 enabled\n");
 #endif
+#ifdef CONFIG_SYS_FSL_ERRATUM_USB138
+	puts("Work-around for Erratum USB138 enabled\n");
+#endif
 	return 0;
 }
 
