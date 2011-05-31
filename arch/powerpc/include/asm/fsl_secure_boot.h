ae904e7f7b3a232c8a6e6b043cf37e186cf2cc53
Kumar Gala <galak@kernel.crashing.org>
2011-05-31 08:53:21 -0500

    Added command for validation of images in case of secure boot
    1. Default environment will be used for secure boot flow
       which can't be edited or saved.
    2. Command for secure boot is predefined in the default
       environment which will run on autoboot (and autoboot is
       the only option allowed in case of secure boot) and it
       looks like this:
       #define CONFIG_SECBOOT \
        "setenv bs_hdraddr 0xe8e00000;"                 \
        "esbc_validate $bs_hdraddr;"                    \
        "source $img_addr;"                             \
        "esbc_halt;"
       #endif
    3. esbc_validate command is meant for validating header and
       signature of images (Boot Script and ESBC uboot client).
       Command usage:
       esbc_validate img_hdr_addr [pub_key_hash]
    4. Boot Script can contain esbc_validate commands and bootm command.
       Uboot source command used in default secure boot command will
       run the bootscript.
    5. ESBC uboot client can be linux. Additionally, rootfs and device
       tree blob can also be signed.
    6. In the event of header or signature failure in validation,
       ITS and ITF bits determine further course of action.
    7. In case of soft failure, appropriate error is dumped on console
       and next esbc_validate command is executed.
    8. In case of hard failure, SoC is issued RESET REQUEST after
       dumping error on the console.
    9. Command esbc_halt added to ensure either bootm executes
       after validation of images or core should just spin.
    
    Signed-off-by: Kuldip Giroh <kuldip.giroh@freescale.com>
    Signed-off-by: Ruchika Gupta <ruchika.gupta@freescale.com>

--- arch/powerpc/include/asm/fsl_secure_boot.h
+++ arch/powerpc/include/asm/fsl_secure_boot.h
@@ -23,11 +23,13 @@
 #ifndef __FSL_SECURE_BOOT_H
 #define __FSL_SECURE_BOOT_H
 
-/* This define indicates that u-boot's code is executing from flash
- * after ISBC Validation */
+/*
+ * This define indicates that u-boot is executing from flash
+ * after ISBC Validation
+ */
 #define CONFIG_SYS_ESBC_FLASH
 
-/*Starting TLB number for the TLB entried for 3.5 G space created by ISBC */
+/* Starting TLB number for the TLB entried for 3.5 G space created by ISBC */
 #define CONFIG_SYS_ISBC_START_TLB		3
 
 /* Number fo TLB's created by ISBC */
@@ -36,5 +38,62 @@
 /* Address map where flash lies as done by PBI commands */
 #define CONFIG_SYS_PBI_FLASH_BASE		0xc0000000
 #define CONFIG_SYS_PBI_FLASH_WINDOW		0xcff80000
+
+/* esbc_validate command in secure_boot would use sha-256 algorithm */
+#define CONFIG_SHA256
+
+/* esbc_validate command in secure_boot would use RSA algorithm */
+#define CONFIG_RSA
+
+/*
+ * Define the key hash for boot script here if public/private key pair used to
+ * sign bootscript are different from the SRK hash put in the fuse
+ * Example of defining KEY_HASH is
+ * #define CONFIG_BOOTSCRIPT_KEY_HASH \
+ *	 "41066b564c6ffcef40ccbc1e0a5d0d519604000c785d97bbefd25e4d288d1c8b"
+ */
+
+#define CONFIG_CMD_ESBC_VALIDATE
+
+/*
+ * Control should not reach back to uboot after validation of images
+ * for secure boot flow and therefore bootscript should have
+ * the bootm command. If control reaches back to uboot anyhow
+ * after validating images, core should just spin.
+ */
+#ifdef CONFIG_BOOTSCRIPT_KEY_HASH
+#define CONFIG_SECBOOT \
+	"setenv bs_hdraddr 0xe8e00000;"			\
+	"esbc_validate $bs_hdraddr "  MK_STR(CONFIG_BOOTSCRIPT_KEY_HASH)";" \
+	"source $img_addr;"					\
+	"esbc_halt;"
+#else
+#define CONFIG_SECBOOT \
+	"setenv bs_hdraddr 0xe8e00000;"			\
+	"esbc_validate $bs_hdraddr;"			\
+	"source $img_addr;"				\
+	"esbc_halt;"
+#endif
+
+/* For secure boot flow, default environment used will be used */
+#undef CONFIG_ENV_IS_IN_FLASH
+#define CONFIG_ENV_IS_NOWHERE
+
+/*
+ * We don't want boot delay for secure boot flow
+ * before autoboot starts
+ */
+#undef CONFIG_BOOTDELAY
+#define CONFIG_BOOTDELAY	0
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND		CONFIG_SECBOOT
+
+/*
+ * CONFIG_ZERO_BOOTDELAY_CHECK should not be defined for
+ * secure boot flow as defining this would enable a user to
+ * reach uboot prompt by pressing some key before start of
+ * autoboot
+ */
+#undef CONFIG_ZERO_BOOTDELAY_CHECK
 
 #endif
