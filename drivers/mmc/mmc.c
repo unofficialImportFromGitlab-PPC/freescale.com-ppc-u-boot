ec2fafd3be4708518382d98a4bd2345dc325d012
Jerry Huang <Chang-Ming.Huang@freescale.com>
2012-03-27 18:28:04 +0800

    SDHC/MMC: remove the member has_init
    When first inserting the SD card to slot, the command "mmcinfo" can
    display the card information correctly.
    But, then removing the SD card or inserting another SD card to slot,
    the command "mmcinfo" can't display the information correctly.
    
    Therefore remove this member 'has_init' from 'structure mmc',
    and add the codes to check the mmc_init, only when mmc_init return
    the right value, driver will print the information.
    
    Below is the error log SD card removed:
    => mmcinfo
    MMC: no card present
    Device: FSL_SDHC
    Manufacturer ID: 3
    OEM: 5344
    Name: SD02G
    Tran Speed: 25000000
    Rd Block Len: 512
    SD version 2.0
    High Capacity: No
    Capacity: 1.8 GiB
    Bus Width: 4-bit
    
    Signed-off-by: Jerry Huang <Chang-Ming.Huang@freescale.com>
    CC: Andy Fleming <afleming@gmail.com>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -1253,13 +1253,9 @@ int mmc_init(struct mmc *mmc)
 	int err;
 
 	if (mmc_getcd(mmc) == 0) {
-		mmc->has_init = 0;
 		printf("MMC: no card present\n");
 		return NO_CARD_ERR;
 	}
-
-	if (mmc->has_init)
-		return 0;
 
 	err = mmc->init(mmc);
 
@@ -1295,10 +1291,7 @@ int mmc_init(struct mmc *mmc)
 	}
 
 	err = mmc_startup(mmc);
-	if (err)
-		mmc->has_init = 0;
-	else
-		mmc->has_init = 1;
+
 	return err;
 }
 
