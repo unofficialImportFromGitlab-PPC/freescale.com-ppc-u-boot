ca970ed184613eb29f24fc3ae461a04db9e7e238
Shaveta Leekha <shaveta@freescale.com>
2012-09-14 12:09:26 +0530

    board/b4860qds:Fix sysclk and ddrclk value print
    As per QXIS FPGA programming Model BRDCFG1 register can be used to read
    the local SYSCLK/DDRCLK clocks settings. where bits brdcfg1[4,5]
    represents sysclk and brdcfg1[6,7] as ddrclk with following bit
    combinations:
        00 :  66.66 MHZ
        01 : 100    MHZ
        10 : 125    MHZ
        11 : 133.33 MHZ
    
    So update sysclk and ddrclk value print functions.
    
    Signed-off-by: Prabhakar Kushwaha <prabhakar@freescale.com>

--- board/freescale/b4860qds/b4860qds_qixis.h
+++ board/freescale/b4860qds/b4860qds_qixis.h
@@ -26,21 +26,11 @@
 #define BRDCFG4_EMISEL_MASK		0xE0
 #define BRDCFG4_EMISEL_SHIFT		5
 
-/* SYSCLK */
-#define QIXIS_SYSCLK_66			0x0
-#define QIXIS_SYSCLK_83			0x1
-#define QIXIS_SYSCLK_100		0x2
-#define QIXIS_SYSCLK_125		0x3
-#define QIXIS_SYSCLK_133		0x4
-#define QIXIS_SYSCLK_150		0x5
-#define QIXIS_SYSCLK_160		0x6
-#define QIXIS_SYSCLK_166		0x7
-
-/* DDRCLK */
-#define QIXIS_DDRCLK_66			0x0
-#define QIXIS_DDRCLK_100		0x1
-#define QIXIS_DDRCLK_125		0x2
-#define QIXIS_DDRCLK_133		0x3
+/* CLK */
+#define QIXIS_CLK_66		0x0
+#define QIXIS_CLK_100		0x1
+#define QIXIS_CLK_125		0x2
+#define QIXIS_CLK_133		0x3
 
 #define QIXIS_SRDS1CLK_122		0x5a
 #define QIXIS_SRDS1CLK_125		0x5e
