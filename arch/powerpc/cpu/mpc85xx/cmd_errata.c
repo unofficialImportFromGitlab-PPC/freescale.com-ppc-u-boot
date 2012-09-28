58e550bb25b926cbfd784b6ebe7fcb1f262abadf
Yuanquan Chen <B41889@freescale.com>
2012-09-28 11:22:00 +0800

    powerpc/p4080ds: Fix PCI-e x8 link training down failure
    If we set the PCI-e controller link width as x8 in rcw and add a
    x1 PCI-e device to it, it fails to train down to x1 width. This
    happens with p4080ds. According to p4080ds errata PCIe-A003, we
    reset the PCI-e controller link width to x4 during the PCI-e init
    in u-boot. Then it can train down to x1 link width.
    
    Signed-off-by: Yuanquan Chen <B41889@freescale.com>

--- arch/powerpc/cpu/mpc85xx/cmd_errata.c
+++ arch/powerpc/cpu/mpc85xx/cmd_errata.c
@@ -161,6 +161,9 @@ static int do_errata(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 #ifdef CONFIG_SYS_FSL_ERRATUM_A004806
 	puts("Work-around for Erratum A004806 enabled\n");
 #endif
+#ifdef CONFIG_SYS_P4080_ERRATUM_PCIE_A003
+	puts("Work-around for Erratum PCIe-A003 enabled\n");
+#endif
 	return 0;
 }
 
