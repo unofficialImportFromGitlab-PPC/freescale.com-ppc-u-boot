38c92d5e6c159abdf2565a0a6ec7ee95c8974094
York Sun <yorksun@freescale.com>
2012-11-07 15:00:57 -0800

    powerpc/mpc8xxx: Fix compiling error caused in DDR driver
    mpc86xx platforms should use CONFIG_SYS_MPC86xx_DDR2_ADDR in utils.c
    if applicable.
    
    Signed-off-by: York Sun <yorksun@freescale.com>

--- arch/powerpc/cpu/mpc8xxx/ddr/util.c
+++ arch/powerpc/cpu/mpc8xxx/ddr/util.c
@@ -152,7 +152,11 @@ void board_add_ram_info(int use_default)
 
 #if CONFIG_NUM_DDR_CONTROLLERS >= 2
 	if (!(sdram_cfg & SDRAM_CFG_MEM_EN)) {
+#ifdef CONFIG_MPC86xx
+		ddr = (void __iomem *)CONFIG_SYS_MPC86xx_DDR2_ADDR;
+#else
 		ddr = (void __iomem *)CONFIG_SYS_MPC85xx_DDR2_ADDR;
+#endif
 		sdram_cfg = in_be32(&ddr->sdram_cfg);
 	}
 #endif
