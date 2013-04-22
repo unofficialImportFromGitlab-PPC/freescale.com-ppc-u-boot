d218ee8993d281a302ba4d549eb497189b251c77
Shaohui Xie <Shaohui.Xie@freescale.com>
2013-04-22 17:28:49 +0800

    powerpc/usb: fix phy_type checking
    strcmp should not be used to check the argument of phy_type which maybe
    parsed by hwconfig_subarg, hwconfig_subarg will return part of hwconfig
    started from the argument if it have the argument, since the phy_type
    should be 'utmi' or 'ulpi', we use strncmp and limit the length to 4.
    
    Signed-off-by: Shaohui Xie <Shaohui.Xie@freescale.com>
    Change-Id: I420f1061cc6f1eeeaad462ca3386ed27f8276508
    Reviewed-on: http://git.am.freescale.net:8181/1668
    Reviewed-by: Sun Yusong-R58495 <yorksun@freescale.com>
    Tested-by: Sun Yusong-R58495 <yorksun@freescale.com>

--- drivers/usb/host/ehci-fsl.c
+++ drivers/usb/host/ehci-fsl.c
@@ -87,7 +87,7 @@ int ehci_hcd_init(int index, struct ehci_hccr **hccr, struct ehci_hcor **hcor)
 #endif
 	}
 
-	if (!strcmp(phy_type, "utmi")) {
+	if (!strncmp(phy_type, "utmi", 4)) {
 #if defined(CONFIG_SYS_FSL_USB_INTERNAL_UTMI_PHY)
 		setbits_be32(&ehci->control, PHY_CLK_SEL_UTMI);
 		setbits_be32(&ehci->control, UTMI_PHY_EN);
