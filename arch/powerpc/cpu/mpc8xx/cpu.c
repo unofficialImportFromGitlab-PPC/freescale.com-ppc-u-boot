b56ea77eaca20f947969979e881c3c23d4dfde71
Scott Wood <scottwood@freescale.com>
2013-05-17 20:01:54 -0500

    powerpc: fix 8xx and 82xx type-punning warnings with GCC 4.7
    C99's strict aliasing rules are insane to use in low-level code such as a
    bootloader, but as Wolfgang has rejected -fno-strict-aliasing in the
    past, add a union so that 16-bit accesses can be performed.
    
    Compile-tested only.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Acked-by: Wolfgang Denk <wd@denx.de>

--- arch/powerpc/cpu/mpc8xx/cpu.c
+++ arch/powerpc/cpu/mpc8xx/cpu.c
@@ -79,7 +79,8 @@ static int check_CPU (long clock, uint pvr, uint immr)
 	if ((pvr >> 16) != 0x0050)
 		return -1;
 
-	k = (immr << 16) | *((ushort *) & immap->im_cpm.cp_dparam[0xB0]);
+	k = (immr << 16) |
+		immap->im_cpm.cp_dparam16[PROFF_REVNUM / sizeof(u16)];
 	m = 0;
 	suf = "";
 
@@ -195,7 +196,8 @@ static int check_CPU (long clock, uint pvr, uint immr)
 	if ((pvr >> 16) != 0x0050)
 		return -1;
 
-	k = (immr << 16) | *((ushort *) & immap->im_cpm.cp_dparam[0xB0]);
+	k = (immr << 16) |
+		immap->im_cpm.cp_dparam16[PROFF_REVNUM / sizeof(u16)];
 	m = 0;
 
 	switch (k) {
@@ -254,7 +256,8 @@ static int check_CPU (long clock, uint pvr, uint immr)
 	if ((pvr >> 16) != 0x0050)
 		return -1;
 
-	k = (immr << 16) | in_be16((ushort *)&immap->im_cpm.cp_dparam[0xB0]);
+	k = (immr << 16) |
+		in_be16(&immap->im_cpm.cp_dparam16[PROFF_REVNUM / sizeof(u16)]);
 	m = 0;
 
 	switch (k) {
@@ -313,7 +316,8 @@ static int check_CPU (long clock, uint pvr, uint immr)
 	if ((pvr >> 16) != 0x0050)
 		return -1;
 
-	k = (immr << 16) | *((ushort *) & immap->im_cpm.cp_dparam[0xB0]);
+	k = (immr << 16) |
+		immap->im_cpm.cp_dparam16[PROFF_REVNUM / sizeof(u16)];
 	m = 0;
 
 	switch (k) {
