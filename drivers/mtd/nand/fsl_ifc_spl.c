c9e05239ba1095ede639a06174ca9886544a7f27
Prabhakar Kushwaha <prabhakar@freescale.com>
2014-02-28 10:58:09 +0530

    driver/ifc: define nand_spl_load_image() for SPL
    nand_spl_load_image() can also be used for non TPL framework.
    
    Signed-off-by: Prabhakar Kushwaha <prabhakar@freescale.com>
    Change-Id: I6b19f309bcb29a14cb9ed3c88b4933f1fdab0359
    Reviewed-on: http://git.am.freescale.net:8181/9468
    Reviewed-by: Shengzhou Liu <Shengzhou.Liu@freescale.com>
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Yusong Sun <yorksun@freescale.com>

--- drivers/mtd/nand/fsl_ifc_spl.c
+++ drivers/mtd/nand/fsl_ifc_spl.c
@@ -88,7 +88,7 @@ static inline int bad_block(uchar *marker, int port_size)
 		return __raw_readw((u16 *)marker) != 0xffff;
 }
 
-#ifdef CONFIG_TPL_BUILD
+#if defined(CONFIG_TPL_BUILD) || defined(CONFIG_SPL_BUILD)
 int nand_spl_load_image(uint32_t offs, unsigned int uboot_size, void *vdst)
 #else
 static int nand_load(uint32_t offs, unsigned int uboot_size, void *vdst)
@@ -221,7 +221,7 @@ static int nand_load(uint32_t offs, unsigned int uboot_size, void *vdst)
  * Defines a static function nand_load_image() here, because non-static makes
  * the code too large for certain SPLs(minimal SPL, maximum size <= 4Kbytes)
  */
-#ifndef CONFIG_TPL_BUILD
+#if !defined(CONFIG_TPL_BUILD) && !defined(CONFIG_SPL_BUILD)
 #define nand_spl_load_image(offs, uboot_size, vdst) \
 	nand_load(offs, uboot_size, vdst)
 #endif
