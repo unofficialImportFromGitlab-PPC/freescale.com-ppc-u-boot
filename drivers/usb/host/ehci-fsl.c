88c73e6d3ea8c4fac76a9546dca5d5e173ad18de
Nikhil Badola <nikhil.badola@freescale.com>
2014-03-24 09:10:02 +0530

    fsl/usb: Fix phy type for second USB controller
    Set correct phy_type value for second USB controller.
    This is required for supporting SOCs having 2 USB controllers
    working simultaneously, one with UTMI phy and other with ULPI phy
    
    Signed-off-by: Nikhil Badola <nikhil.badola@freescale.com>
    Change-Id: Ib2a1696898989c6e5040ba339197e10b67b52b12
    Reviewed-on: http://git.am.freescale.net:8181/7110
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Ramneek Mehresh <ramneek.mehresh@freescale.com>
    Reviewed-by: Yusong Sun <yorksun@freescale.com>

--- drivers/usb/host/ehci-fsl.c
+++ drivers/usb/host/ehci-fsl.c
@@ -46,11 +46,15 @@ int ehci_hcd_init(int index, enum usb_init_type init,
 	struct usb_ehci *ehci = NULL;
 	const char *phy_type = NULL;
 	size_t len;
+	char current_usb_controller[5];
 #ifdef CONFIG_SYS_FSL_USB_INTERNAL_UTMI_PHY
 	char usb_phy[5];
 
 	usb_phy[0] = '\0';
 #endif
+	memset(current_usb_controller, '\0', 5);
+	snprintf(current_usb_controller, 4, "usb%d", index+1);
+
 	if (has_erratum_a007075())
 		/*
 		 * A 5ms delay is needed after applying soft-reset to the
@@ -83,8 +87,9 @@ int ehci_hcd_init(int index, enum usb_init_type init,
 	out_be32(&ehci->snoop2, 0x80000000 | SNOOP_SIZE_2GB);
 
 	/* Init phy */
-	if (hwconfig_sub("usb1", "phy_type"))
-		phy_type = hwconfig_subarg("usb1", "phy_type", &len);
+	if (hwconfig_sub(current_usb_controller, "phy_type"))
+		phy_type = hwconfig_subarg(current_usb_controller,
+				"phy_type", &len);
 	else
 		phy_type = getenv("usb_phy_type");
 
