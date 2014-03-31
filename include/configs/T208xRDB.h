0ef0888a4f857412cf311b9c1bd54b046ac3d925
Shengzhou Liu <Shengzhou.Liu@freescale.com>
2014-03-31 16:35:00 +0800

    board/t2080rdb: fix CONFIG_SYS_I2C_EEPROM_ADDR_LEN
    T2080RDB uses a 32K bytes EEPROM(AT24C256), on which random word addressing
    requires a 15-bit data word address, so fix CONFIG_SYS_I2C_EEPROM_ADDR_LEN
    to 2.
    
    Signed-off-by: Shengzhou Liu <Shengzhou.Liu@freescale.com>
    Change-Id: Ia188dbdb522eb22682cf9f291ff5bb00afe90389
    Reviewed-on: http://git.am.freescale.net:8181/10511
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Xiaobo Xie <X.Xie@freescale.com>
    Reviewed-by: Yusong Sun <yorksun@freescale.com>

--- include/configs/T208xRDB.h
+++ include/configs/T208xRDB.h
@@ -209,7 +209,7 @@ unsigned long get_board_ddr_clk(void);
 #define CONFIG_SYS_I2C_EEPROM_NXID
 #define CONFIG_SYS_EEPROM_BUS_NUM	0
 #define CONFIG_SYS_I2C_EEPROM_ADDR	0x50
-#define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	1
+#define CONFIG_SYS_I2C_EEPROM_ADDR_LEN	2
 
 /*
  * DDR Setup
