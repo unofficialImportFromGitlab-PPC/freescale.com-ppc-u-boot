44fd2af5b89b14a3748875c8c6e3e44c34d4140e
Prabhakar Kushwaha <prabhakar@freescale.com>
2014-04-01 09:16:29 +0530

    powerpc/b4860:Drop errata A-005936 workaround
    Freescale is removing support of b4 rev1 SoC.
    
    As Errata A-005936 is only applicable for b4 rev1, drop this workaround.
    
    Signed-off-by: Prabhakar Kushwaha <prabhakar@freescale.com>
    Change-Id: I2e8114291bdcfddd9af53e68de0014f82c4a2e60
    Reviewed-on: http://git.am.freescale.net:8181/8915
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Yusong Sun <yorksun@freescale.com>

--- arch/powerpc/cpu/mpc85xx/cmd_errata.c
+++ arch/powerpc/cpu/mpc85xx/cmd_errata.c
@@ -324,9 +324,6 @@ static int do_errata(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	if (has_erratum_a006379())
 		puts("Work-around for Erratum A006379 enabled\n");
 #endif
-#if defined(CONFIG_SYS_FSL_ERRATUM_A005936) && defined(CONFIG_B4860QDS)
-	puts("Work-around for Erratum A005936 enabled\n");
-#endif
 #if defined(CONFIG_SYS_FSL_B4860QDS_XFI_ERR) && defined(CONFIG_B4860QDS)
 	puts("Work-around for Erratum XFI on B4860QDS enabled\n");
 #endif
