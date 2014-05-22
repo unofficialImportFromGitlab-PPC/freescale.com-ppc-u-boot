7114868baec322017fcd637700152f3801b9c438
York Sun <yorksun@freescale.com>
2014-05-22 09:28:35 -0700

    powerpc/mpc85xx: Add workaround for DDR erratum A004508
    When the DDR controller is initialized below a junction temperature of
    0°C and then operated above a junction temperature of 65°C, the DDR
    controller may cause receive data errors, resulting ECC errors and/or
    corrupted data. This erratum applies to the following SoCs and their
    variants: MPC8536, MPC8569, MPC8572, P1010, P1020, P1021, P1022, P1023,
    P2020.
    
    Signed-off-by: York Sun <yorksun@freescale.com>
    Change-Id: I833e00426404fc1c95c0ad120c833fa2a0bdd051
    Reviewed-on: http://git.am.freescale.net:8181/12824
    Reviewed-by: James Yang <James.Yang@freescale.com>
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>

--- arch/powerpc/cpu/mpc85xx/cmd_errata.c
+++ arch/powerpc/cpu/mpc85xx/cmd_errata.c
@@ -240,6 +240,9 @@ static int do_errata(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	if (SVR_MAJ(svr) == 1)
 		puts("Work-around for Erratum A004390 enabled\n");
 #endif
+#ifdef CONFIG_SYS_FSL_ERRATUM_A004508
+	puts("Work-around for Erratum A004508 enabled\n");
+#endif
 #ifdef CONFIG_SYS_FSL_ERRATUM_A004510
 	puts("Work-around for Erratum A004510 enabled\n");
 #endif
