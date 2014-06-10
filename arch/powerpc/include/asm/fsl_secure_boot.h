3fae402ba55caaf507e185e69ed4d2d8c29b26ee
Shengzhou Liu <Shengzhou.Liu@freescale.com>
2014-06-10 17:35:15 +0800

    powerpc/mpc85xx: Add T1024/T1023 SoC support
    Add support for Freescale T1024/T1023 SoC.
    
    The T1024 SoC includes the following function and features:
    - Two 64-bit Power architecture e5500 cores, up to 1.4GHz
    - private 256KB L2 cache each core and shared 256KB CoreNet platform cache (CPC)
    - 32-/64-bit DDR3L/DDR4 SDRAM memory controller with ECC and interleaving support
    - Data Path Acceleration Architecture (DPAA) incorporating acceleration
    - Four MAC for 1G/2.5G/10G network interfaces (RGMII, SGMII, QSGMII, XFI)
    - High-speed peripheral interfaces
      - Three PCI Express 2.0 controllers
    - Additional peripheral interfaces
      - One SATA 2.0 controller
      - Two USB 2.0 controllers with integrated PHY
      - Enhanced secure digital host controller (SD/eSDHC/eMMC)
      - Enhanced serial peripheral interface (eSPI)
      - Four I2C controllers
      - Four 2-pin UARTs or two 4-pin UARTs
      - Integrated Flash Controller supporting NAND and NOR flash
    - Two 8-channel DMA engines
    - Multicore programmable interrupt controller (PIC)
    - LCD interface (DIU) with 12 bit dual data rate
    - QUICC Engine block supporting TDM, HDLC, and UART
    - Deep Sleep power implementaion (wakeup from GPIO/Timer/Ethernet/USB)
    - Support for hardware virtualization and partitioning enforcement
    - QorIQ Platform's Trust Architecture 2.0
    
    Differences between T1024 and T1023:
      Feature         T1024  T1023
      QUICC Engine:   yes    no
      DIU:            yes    no
      Deep Sleep:     yes    no
      I2C controller: 4      3
      DDR:            64-bit 32-bit
      IFC:            32-bit 28-bit
    
    Signed-off-by: Shengzhou Liu <Shengzhou.Liu@freescale.com>
    Change-Id: I65a05e6941bb828c212d7e811a9386d1e249e2f9
    Reviewed-on: http://git.am.freescale.net:8181/17042
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Xiaobo Xie <X.Xie@freescale.com>
    Reviewed-by: Yusong Sun <yorksun@freescale.com>

--- arch/powerpc/include/asm/fsl_secure_boot.h
+++ arch/powerpc/include/asm/fsl_secure_boot.h
@@ -35,7 +35,9 @@
 	defined(CONFIG_T2080QDS) || \
 	defined(CONFIG_T2080RDB) || \
 	defined(CONFIG_T1040QDS) || \
-	defined(CONFIG_T104xRDB)
+	defined(CONFIG_T104xRDB) || \
+	defined(CONFIG_PPC_T1024) || \
+	defined(CONFIG_PPC_T1023)
 #define CONFIG_SYS_CPC_REINIT_F
 #define CONFIG_KEY_REVOCATION
 #undef CONFIG_SYS_INIT_L3_ADDR
