546467135e9b10277e3c3ba21f7c719272f0e67b
Jeffrey Ladouceur <Jeffrey.Ladouceur@freescale.com>
2014-12-07 09:42:22 -0500

    qbman: inhibit qman and bman portals by default
    Not all portals might be managed and therefore visible.
    Set the isdr register so that the corresponding isr register won't be set.
    This is required to support power management.
    
    Change-Id: Id2d4175b08a841b36bdd2dd631fce7de3e7fcd7f
    Signed-off-by: Jeffrey Ladouceur <Jeffrey.Ladouceur@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/25448
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Geoff Thorpe <Geoff.Thorpe@freescale.com>
    Reviewed-by: Yusong Sun <yorksun@freescale.com>

--- arch/powerpc/cpu/mpc85xx/portals.c
+++ arch/powerpc/cpu/mpc85xx/portals.c
@@ -14,33 +14,33 @@
 #include <asm/fsl_portals.h>
 #include <asm/fsl_liodn.h>
 
-#define MAX_PORTALS (CONFIG_SYS_BMAN_CINH_SIZE / CONFIG_SYS_BMAN_SP_CINH_SIZE)
-void inhibit_bman_portals(void)
+#define MAX_BPORTALS (CONFIG_SYS_BMAN_CINH_SIZE / CONFIG_SYS_BMAN_SP_CINH_SIZE)
+#define MAX_QPORTALS (CONFIG_SYS_QMAN_CINH_SIZE / CONFIG_SYS_QMAN_SP_CINH_SIZE)
+static void inhibit_portals(void __iomem *addr, int max_portals,
+			int arch_max_portals, int portal_cinh_size)
 {
-	void __iomem *addr = (void *)CONFIG_SYS_BMAN_CINH_BASE +
-		CONFIG_SYS_BMAN_SWP_ISDR_REG;
 	uint32_t val;
-	int i, max_portals = CONFIG_SYS_BMAN_NUM_PORTALS;
+	int i;
 
-	/* MAX_PORTALS is the maximum based on memory size. This includes the
-	 * reserved memory in the SoC.  CONFIG_SYS_BMAN_NUM_PORTALS is the
-	 * number of physical portals in the SoC */
-	if (CONFIG_SYS_BMAN_NUM_PORTALS > MAX_PORTALS) {
-		printf("ERROR: BMAN portal config error\n");
-		max_portals = MAX_PORTALS;
+	/* arch_max_portals is the maximum based on memory size. This includes
+	 * the reserved memory in the SoC.  max_portals the number of physical
+	 * portals in the SoC */
+	if (max_portals > arch_max_portals) {
+		printf("ERROR: portal config error\n");
+		max_portals = arch_max_portals;
 	}
 
 	for (i = 0; i < max_portals; i++) {
 		out_be32(addr, -1);
 		val = in_be32(addr);
 		if (!val) {
-			printf("ERROR: Stopped after %d BMan portals\n", i);
+			printf("ERROR: Stopped after %d portals\n", i);
 			goto done;
 		}
-		addr += CONFIG_SYS_BMAN_SP_CINH_SIZE;
+		addr += portal_cinh_size;
 	}
 #ifdef DEBUG
-	printf("Cleared %d Bman portals\n", i);
+	printf("Cleared %d portals\n", i);
 #endif
 done:
 
@@ -50,6 +50,10 @@ done:
 void setup_portals(void)
 {
 	ccsr_qman_t *qman = (void *)CONFIG_SYS_FSL_QMAN_ADDR;
+	void __iomem *bpaddr = (void *)CONFIG_SYS_BMAN_CINH_BASE +
+				CONFIG_SYS_BMAN_SWP_ISDR_REG;
+	void __iomem *qpaddr = (void *)CONFIG_SYS_QMAN_CINH_BASE +
+				CONFIG_SYS_QMAN_SWP_ISDR_REG;
 #ifdef CONFIG_FSL_CORENET
 	int i;
 
@@ -73,7 +77,10 @@ void setup_portals(void)
 	out_be32(&qman->qcsp_bar, (u32)CONFIG_SYS_QMAN_MEM_PHYS);
 
 	/* Change default state of BMan ISDR portals to all 1s */
-	inhibit_bman_portals();
+	inhibit_portals(bpaddr, CONFIG_SYS_BMAN_NUM_PORTALS, MAX_BPORTALS,
+			CONFIG_SYS_BMAN_SP_CINH_SIZE);
+	inhibit_portals(qpaddr, CONFIG_SYS_QMAN_NUM_PORTALS, MAX_QPORTALS,
+			CONFIG_SYS_QMAN_SP_CINH_SIZE);
 }
 
 /* Update portal containter to match LAW setup of portal in phy map */
