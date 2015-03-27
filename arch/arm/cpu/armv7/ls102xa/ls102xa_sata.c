575e2809e026de2e4c7fb456ac3bfba39ea14a44
Tang Yuantian <Yuantian.Tang@freescale.com>
2015-03-27 17:24:36 +0800

    ls1021a: ahci: Added workaround for erratum ERR008407
    The workaround of erratum ERR008407
    Description:
        A small on chip SRAM inside SATA controller has ECC
        protection circuit. Due to a logic problem, ECC circuit
        generates incorrect ECC error, which is reported as
        CRC error.
    
    Workaround:
        Disable ECC checking for the SRAM by writing to address
        0x20220520 with value 0x00020000. The CRC checking for
        the frame works normally after ECC disabling.
    
    Signed-off-by: Shaohui Xie <Shaohui.Xie@freescale.com>
    Signed-off-by: Tang Yuantian <Yuantian.Tang@freescale.com>
    Change-Id: Ib34b4d4b5ecc4837ff430887ae4a2251ba303a09
    Reviewed-on: http://git.am.freescale.net:8181/33683
    Reviewed-by: Zhengxiong Jin <Jason.Jin@freescale.com>
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Prabhakar Kushwaha <prabhakar@freescale.com>

--- arch/arm/cpu/armv7/ls102xa/ls102xa_sata.c
+++ arch/arm/cpu/armv7/ls102xa/ls102xa_sata.c
@@ -13,12 +13,20 @@ int ls1021a_sata_init(void)
 {
 	struct ccsr_ahci __iomem *ccsr_ahci = (void *)AHCI_BASE_ADDR;
 
+#ifdef CONFIG_SYS_FSL_ERRATUM_A008407
+	unsigned int __iomem *dcfg_ecc = (void *)0x20220520;
+#endif
+
 	out_le32(&ccsr_ahci->ppcfg, 0xa003fffe);
 	out_le32(&ccsr_ahci->pp2c, 0x28183411);
 	out_le32(&ccsr_ahci->pp3c, 0x0e081004);
 	out_le32(&ccsr_ahci->pp4c, 0x00480811);
 	out_le32(&ccsr_ahci->pp5c, 0x192c96a4);
 	out_le32(&ccsr_ahci->ptc, 0x08000024);
+
+#ifdef CONFIG_SYS_FSL_ERRATUM_A008407
+	out_le32(dcfg_ecc, 0x00020000);
+#endif
 
 	return 0;
 }
