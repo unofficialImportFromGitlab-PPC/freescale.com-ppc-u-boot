d3e6f119a0df614fde5f6cd6bcfa12c6da3a4c19
Horia Geantă <horia.geanta@freescale.com>
2015-07-03 20:07:30 +0300

    Revert "Enable snooping on transactions from CAAM block"
    This reverts commit af1dbffcef3b0c07d5a83327cfd88b269db1b880.
    
    Since writing to SCFG_SNPCNFGCR register has been fixed
    in LS1021 Rev2, programming the SMMU to modify CAAM transaction
    attributes (to enable snooping) is no longer needed.
    
    Change-Id: I70265c60c9b66069b30c4385f30f4b5677901951
    Signed-off-by: Horia Geantă <horia.geanta@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/39260
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Zhengxiong Jin <Jason.Jin@freescale.com>

--- arch/arm/include/asm/arch-ls102xa/ls102xa_stream_id.h
+++ arch/arm/include/asm/arch-ls102xa/ls102xa_stream_id.h
@@ -6,39 +6,6 @@
 
 #ifndef __FSL_LS102XA_STREAM_ID_H_
 #define __FSL_LS102XA_STREAM_ID_H_
-#define CONFIG_SMMU_NSCR_OFFSET		0x400
-#define CONFIG_SMMU_SMR_OFFSET		0x800
-#define CONFIG_SMMU_S2CR_OFFSET		0xc00
-
-#define SMMU_NSCR_CLIENTPD_SHIFT	0
-#define SMMU_NSCR_MTCFG_SHIFT		20
-
-#define SMR_SMR_VALID_SHIFT		31
-#define SMR_ID_MASK			0x7fff
-#define SMR_MASK_SHIFT			16
-
-#define S2CR_WACFG_SHIFT		22
-#define S2CR_WACFG_MASK			0x3
-#define S2CR_WACFG_WRITE_ALLOCATE	0x2
-
-#define S2CR_RACFG_SHIFT		20
-#define S2CR_RACFG_MASK			0x3
-#define S2CR_RACFG_READ_ALLOCATE	0x2
-
-#define S2CR_TYPE_SHIFT			16
-#define S2CR_TYPE_MASK			0x3
-#define S2CR_TYPE_BYPASS		0x01
-
-#define S2CR_MEM_ATTR_SHIFT		12
-#define S2CR_MEM_ATTR_MASK		0xf
-#define S2CR_MEM_ATTR_CACHEABLE		0xa
-
-#define S2CR_MTCFG			0x00000800
-
-#define S2CR_SHCFG_SHIFT		8
-#define S2CR_SHCFG_MASK			0x3
-#define S2CR_SHCFG_OUTER_CACHEABLE	0x1
-#define S2CR_SHCFG_INNER_CACHEABLE	0x2
 
 #include <fsl_sec.h>
 
@@ -104,5 +71,4 @@ struct smmu_stream_id {
 
 void ls1021x_config_caam_stream_id(struct liodn_id_table *tbl, int size);
 void ls102xa_config_smmu_stream_id(struct smmu_stream_id *id, uint32_t num);
-void ls1021x_config_smmu3(uint32_t liodn);
 #endif
