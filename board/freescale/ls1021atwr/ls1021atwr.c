449ab6a3f403f0e4374244d5166ea4ca03796c86
Yuan Yao <yao.yuan@freescale.com>
2015-07-07 15:52:24 +0800

    ls102xa: Enable snoop and DVM message requests.
    Signed-off-by: Yuan Yao <yao.yuan@freescale.com>
    Change-Id: I8f579add5d0cc2005d36b3588285141adaab443e
    Reviewed-on: http://git.am.freescale.net:8181/39355
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Zhengxiong Jin <Jason.Jin@freescale.com>

--- board/freescale/ls1021atwr/ls1021atwr.c
+++ board/freescale/ls1021atwr/ls1021atwr.c
@@ -417,8 +417,14 @@ int board_early_init_f(void)
 
 	/*
 	 * Enable snoop requests and DVM message requests for
-	 * Slave insterface S4 (A7 core cluster)
+	 * All the slave insterfaces.
 	 */
+	out_le32(&cci->slave[0].snoop_ctrl,
+		 CCI400_DVM_MESSAGE_REQ_EN | CCI400_SNOOP_REQ_EN);
+	out_le32(&cci->slave[1].snoop_ctrl,
+		 CCI400_DVM_MESSAGE_REQ_EN | CCI400_SNOOP_REQ_EN);
+	out_le32(&cci->slave[2].snoop_ctrl,
+		 CCI400_DVM_MESSAGE_REQ_EN | CCI400_SNOOP_REQ_EN);
 	out_le32(&cci->slave[4].snoop_ctrl,
 		 CCI400_DVM_MESSAGE_REQ_EN | CCI400_SNOOP_REQ_EN);
 
