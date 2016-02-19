5ba612cec8cfd94053f69b13353bde7af5d7d058
Sriram Dash <sriram.dash@nxp.com>
2016-02-19 15:41:48 +0530

    armv7: Add workaround for USB erratum A-009798
    USB High Speed Squelch Threshold Adjustment
    This patch is adding the erratum for LS1021.
    
    Signed-off-by: Sriram Dash <sriram.dash@nxp.com>
    Signed-off-by: Rajesh Bhagat <rajesh.bhagat@nxp.com>

--- arch/arm/cpu/armv7/ls102xa/soc.c
+++ arch/arm/cpu/armv7/ls102xa/soc.c
@@ -31,6 +31,15 @@ static void erratum_a009008(void)
 #endif /* CONFIG_SYS_FSL_ERRATUM_A009008 */
 }
 
+static void erratum_a009798(void)
+{
+#ifdef CONFIG_SYS_FSL_ERRATUM_A009798
+	u32 __iomem *scfg = (u32 __iomem *)SCFG_BASE;
+	u32 val = in_be32(scfg + SCFG_USB3PRM1CR / 4);
+	out_be32(scfg + SCFG_USB3PRM1CR / 4, val & USB_SQRXTUNE);
+#endif /* CONFIG_SYS_FSL_ERRATUM_A009798 */
+}
+
 int arch_soc_init(void)
 {
 	struct ccsr_scfg *scfg = (struct ccsr_scfg *)CONFIG_SYS_FSL_SCFG_ADDR;
@@ -98,5 +107,6 @@ int arch_soc_init(void)
 
 	/* Erratum */
 	erratum_a009008();
+	erratum_a009798();
 	return 0;
 }
